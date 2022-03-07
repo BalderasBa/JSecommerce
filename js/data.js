2df0ee8b27278cd42a009a85e128f07e0fd99240
Yehya <40221838+BalderasBa@users.noreply.github.com>
2022-03-07 02:09:36 +0100

    assets=links

--- js/data.js
+++ js/data.js
@@ -1,41 +1,42 @@
 let productsDB = [
   {
     id: 1,
-    title: "HeadPhone",
+    title: "RG BA",
     desc: "Lorem ipsum dolor sit amet.",
     size: "large",
-    imageUrl: "../assets/csq (1).jpg",
+    imageUrl: "https://i.ibb.co/LCZJSms/RG-B-A-1800x1800.jpg",
     qty: 1,
     ismine: false,
   },
 
   {
     id: 2,
-    title: "Watch",
+    title: "Origen Sahara V2",
     desc: "Lorem ipsum dolor sit amet.",
     size: "small",
-    imageUrl: "../assets/NewKWatch.jpg",
+    imageUrl: "https://i.ibb.co/NpJzHCV/Origen-Sahara-Date-V2-1800x1800.jpg",
     qty: 1,
     ismine: false,
   },
   {
     id: 3,
-    title: "Head2",
+    title: "N200 BND 2021",
     desc: "Lorem ipsum dolor sit amet.",
     size: "medium",
-    imageUrl: "../assets/csq (2).jpg",
+    imageUrl: "https://i.ibb.co/kSXRtWf/N200-BND2021-gray-1-1800x1800.jpg",
     qty: 1,
     ismine: false,
   },
   {
     id: 4,
-    title: "Laptop",
+    title: "Oman LEn200",
     desc: "Lorem ipsum dolor sit amet.",
     size: "large",
-    imageUrl: "../assets/laptop.jpg",
+    imageUrl:
+      "https://i.ibb.co/4fGTsQn/Oman-LEn200-0003-IMG-00191-1800x1800.jpg",
     qty: 1,
     ismine: false,
   },
 ];
 
-// localStorage.setItem("products", JSON.stringify(productsDB));
+localStorage.setItem("products", JSON.stringify(productsDB));
